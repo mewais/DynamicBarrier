7e8b3cfdf1e111b259a320f1803e5dd605d57bf4
Mohammad Ewais <mohammad.a.ewais@gmail.com>
2024-03-21 01:22:59 -0400

    Initial commit with header

---
+++ include/DynamicBarrier.hpp
@@ -0,0 +1,109 @@
+#ifndef __DYNAMICBARRIER_HPP__
+#define __DYNAMICBARRIER_HPP__
+
+#include <cstdint>
+#include <atomic>
+
+namespace DYNBAR
+{
+    class DynamicBarrier
+    {
+        private:
+            struct alignas(8) Payload
+            {
+                uint32_t target;
+                uint32_t count;
+
+                Payload() : target(0), count(0)
+                {
+                }
+
+                Payload(uint32_t count, uint32_t target) : target(target), count(count)
+                {
+                }
+            };
+
+            std::atomic<Payload> payload;
+
+        public:
+            DynamicBarrier() : payload(Payload(0, 0))
+            {
+            }
+
+            void IncrementTarget()
+            {
+                // Can only increment the target if the barrier is NOT in use (i.e., count == 0).
+                Payload old_payload = this->payload.load();
+                old_payload.count = 0;
+                Payload new_payload = old_payload;
+                new_payload.target++;
+                while (!this->payload.compare_exchange_weak(old_payload, new_payload))
+                {
+                    old_payload.count = 0;
+                    new_payload = old_payload;
+                    new_payload.target++;
+                }
+            }
+
+            void DecrementTarget()
+            {
+                // To avoid deadlocks, decrementing a target can happen at any time, as long as count is less than target.
+                // To elaborate on deadlocks, imaging the following scenario:
+                // 1. Thread 1 enters.
+                // 2. Thread 2 tries to decrement, has to wait for all to exit barrier.
+                // 3. Thread 1 will never exit barrier because it is waiting for thread 2 to enter.
+                // 4. Deadlock.
+                Payload old_payload = this->payload.load();
+                while (old_payload.count == old_payload.target)
+                {
+                    old_payload = this->payload.load();
+                }
+                Payload new_payload = old_payload;
+                new_payload.target--;
+                while (!this->payload.compare_exchange_weak(old_payload, new_payload))
+                {
+                    while (old_payload.count == old_payload.target)
+                    {
+                        old_payload = this->payload.load();
+                    }
+                    new_payload = old_payload;
+                    new_payload.target--;
+                }
+            }
+
+            uint32_t Arrive()
+            {
+                // Enter the barrier
+                Payload old_payload = this->payload.load();
+                Payload new_payload = old_payload;
+                new_payload.count++;
+                while (!this->payload.compare_exchange_weak(old_payload, new_payload))
+                {
+                    new_payload = old_payload;
+                    new_payload.count++;
+                }
+                if (old_payload.count == 0)
+                {
+                    // First one to enter, wait for target to be reached then reset count.
+                    old_payload.count = old_payload.target;
+                    new_payload.count = 0;
+                    while (!this->payload.compare_exchange_weak(old_payload, new_payload))
+                    {
+                        old_payload.count = old_payload.target;
+                        new_payload.target = old_payload.target;
+                        new_payload.count = 0;
+                    }
+                    return 0;
+                }
+                else
+                {
+                    // Not the first one to enter, wait for count to be reset.
+                    while (this->payload.load().count != 0);
+                    // Return order of entry.
+                    return old_payload.count;
+                }
+            }
+    };
+}
+
+#endif //__DYNAMICBARRIER_HPP__
